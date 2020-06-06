17014fc8c13b1a6e88f163f22f1e71a3aa55fffd
bjdix <bdbennison@gmail.com>
2020-06-06 17:34:31 -0500

    Completed module Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = Enums.GradeBookType.Ranked;
+        }        
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
