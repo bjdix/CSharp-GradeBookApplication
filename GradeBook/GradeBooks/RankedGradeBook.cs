492b6e63df92331190dc0e081eb296b29854660a
bjdix <bdbennison@gmail.com>
2020-06-07 10:02:15 -0500

    Added override methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,7 +31,20 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
+        }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new System.InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new System.InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+            base.CalculateStudentStatistics(name);
         }
     }
 }
